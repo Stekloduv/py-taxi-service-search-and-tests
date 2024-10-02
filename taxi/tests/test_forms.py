de8d43777406973c5ae263ca1ec71b8a9f750763
Oleksandr Mykhailyk <stekloduv45@gmail.com>
2024-10-02 12:28:47 +0200

    done

---
+++ taxi/tests/test_forms.py
@@ -0,0 +1,41 @@
+from django.test import TestCase
+from taxi.forms import (
+    DriverCreationForm,
+    CarSearchForm,
+    DriverSearchForm,
+    ManufacturerSearchForm
+)
+
+
+class FormTests(TestCase):
+
+    def test_driver_creation_form_invalid(self):
+        form_data = {
+            "license_number": "test_license_number",
+            "first_name": "test_first_name",
+            "last_name": "test_last_name",
+        }
+        form = DriverCreationForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("license_number", form.errors)
+
+    def test_car_search_form(self):
+        form_data = {
+            "model": "test_model"
+        }
+        form = CarSearchForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_driver_search_form(self):
+        form_data = {
+            "username": "test_username",
+        }
+        form = DriverSearchForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_manufacturer_search_form(self):
+        form_data = {
+            "name": "test_model"
+        }
+        form = ManufacturerSearchForm(data=form_data)
+        self.assertTrue(form.is_valid())
--- taxi/tests/tests_models.py
+++ taxi/tests/tests_models.py
@@ -24,6 +24,21 @@ class ModelTests(TestCase):
             f"({driver.first_name} "
             f"{driver.last_name})")
 
+    def test_driver_with_license_number(self):
+        username = "test_username"
+        password = "test1234"
+        license_number = "AAA54321"
+
+        driver = get_user_model().objects.create_user(
+            username=username,
+            password=password,
+            license_number=license_number,
+        )
+
+        self.assertEqual(driver.username, username)
+        self.assertTrue(driver.check_password(password))
+        self.assertEqual(driver.license_number, license_number)
+
     def test_driver_root_address(self):
         driver = get_user_model().objects.create(
             username="test",
